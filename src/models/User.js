42b68aab217bfd0729cf8399d79bffa1cdb9682a
yhyalp <yhyalpakcy@hotmail.com>
2025-03-29 14:14:28 +0300

    ddd

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   username: String,
-  password: String, // Store hashed passwords for security
-  role: { type: String, enum: ["admin", "user"], default: "user" },
+  password: String, // Store hashed passwords in production
+  isAdmin: Boolean,
+  ratings: [{ itemId: mongoose.Schema.Types.ObjectId, rating: Number }],
+  reviews: [{ itemId: mongoose.Schema.Types.ObjectId, review: String }],
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
