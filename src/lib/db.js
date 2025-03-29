b09f7d6ec2c4fd75e246ead4207f06c575a1dca9
yhyalp <yhyalpakcy@hotmail.com>
2025-03-29 13:35:57 +0300

    aaaa

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    if (mongoose.connections[0].readyState) {
-      console.log("Already connected to MongoDB");
-      return;
-    }
+  if (mongoose.connections[0].readyState) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
 
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Database connection error:", error);
